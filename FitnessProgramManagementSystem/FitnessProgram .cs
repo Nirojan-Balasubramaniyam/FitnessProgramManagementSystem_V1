caacf4fb66896b092b099293fcd1ccfc448c82e2
ut01170 <ut01170@UNICOMTIC14>
2024-10-11 10:02:13 +0530

    2nd commit- fitnessprogram class created

---
+++ FitnessProgramManagementSystem/FitnessProgram .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgram
+    {
+        private string fitnessProgramId { get; set; }
+        private string title { get; set; }
+        private string duration { get; set; }
+        private string price { get; set; }
+
+    }
+}
